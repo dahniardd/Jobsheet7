89155327813d556b9826cf8ea2f921d3f6fe451b
dahniardd <dahniardvn09@gmail.com>
2024-03-27 21:06:08 +0700

    Sorting 06

---
+++ minggu7/Sorting06.java
@@ -0,0 +1,18 @@
+package minggu7;
+import java.util.Scanner;
+
+public class Sorting06 {
+    public static void sequentialSearch(int[] arr, int key){
+        for(int i = 0; i < arr.length; i++) {
+            if (arr [i] == key) {
+                System.out.println("Data ditemukan pada indeks ke- " +i);
+                return;
+            }
+        }
+        System.out.println("Data tidak ditemukan");
+    }
+    public static void main(String[]args) {
+        int[] daftarNilai = { 10, 5, 20, 15, 5, 45 };
+        sequentialSearch(daftarNilai, 5);
+    }
+}
